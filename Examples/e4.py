d5e3aa3787c1f6423f550bd619b594268b113f64
SatomiRen <kozhukhovfilipp@gmail.com>
2022-04-14 11:59:11 +0300

    Commit 6

--- Examples/e4.py
+++ Examples/e4.py
@@ -1,10 +1,8 @@
-# open the file2.txt in read mode. causes error if no such file exists.
+# open the fil2.txt in read mode. causes error if no such file exists.
 fileptr = open("file2.txt", "r")
 # stores all the data of the file into the variable content
-content1 = fileptr.readline()
-content2 = fileptr.readline()
+content = fileptr.readlines()
 # prints the content of the file
-print(content1)
-print(content2)
+print(content)
 # closes the opened file
 fileptr.close()
