7a5160fa784afaab46b2575e5596ed36a4618d35
SatomiRen <64731789+CMDR-Inferion@users.noreply.github.com>
2022-04-14 19:08:28 +0300

    Commit 24

--- Individual/idz1.py
+++ Individual/idz1.py
@@ -4,10 +4,10 @@ import re
 
 if __name__ == "__main__":
     with open('../Examples/text.txt', encoding='UTF') as f:
-    while True:
-        line = f.readline()
-        if len(line) == 0:  # Нулевая длина обозначает конец файла
-            break
-        if not re.findall(r'\d{2}', line):
-            print(line)
-    f.close()  # закрываем файл
+        while True:
+            line = f.readline()
+            if len(line) == 0:  # Нулевая длина обозначает конец файла
+                break
+            if not re.findall(r'\d{2}', line):
+                print(line)
+        f.close()  # закрываем файл
