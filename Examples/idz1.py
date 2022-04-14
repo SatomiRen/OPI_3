237bd505033948b8c572741e1fdb967b0f140fe3
SatomiRen <kozhukhovfilipp@gmail.com>
2022-04-14 14:30:05 +0300

    Commit 21

---
+++ Examples/idz1.py
@@ -0,0 +1,11 @@
+import re
+f = open('text.txt', encoding='UTF')
+
+while True:
+    line = f.readline()
+    if len(line) == 0:  # Нулевая длина обозначает конец файла
+        break
+    if not re.findall(r'\d{2}', line):
+        print(line)
+
+f.close()  # закрываем файл
